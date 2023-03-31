3c5cbe863351196195c680b8cec42445fa93e753
stephen0603 <stephen850603@gmail.com>
2023-03-31 17:21:51 +0800

    git init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import c from './c.js';
+import css from "./style.scss";
+import axios from 'axios';
+axios.get("https://hexschool.github.io/ajaxHomework/data.json").then(function(response){
+    console.log(response);
+})
+let a = 1;
+let b = 2;
+console.log("hello");
+function hello(a,b){
+    console.log(a+b);
+}
+hello(1,2);
+console.log(c);
